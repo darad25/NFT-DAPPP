4ce67202fa3e3a2a0242096cbf4b2e0b50afb964
1amdara <77836449+1amdara@users.noreply.github.com>
2023-11-22 14:25:17 -0800

    first commit

--- src/utils/walletConnect/walletConnect.utils.js
+++ src/utils/walletConnect/walletConnect.utils.js
@@ -44,27 +44,4 @@ import { useState } from "react";
     //using hardhat if you run 'npx hardhat compile' you get your abis generated for you in your artifacts folder in your repo
     //now i will call and send tx using NFT.sol
   
-    const mint = async () => {
-      const Labi = abi.Labi;
-      const contractAddress = "ffcac71d0d7ae3fe567eb27fc05800c560a126543d5006f9cde84e9a496bfe0f"; //addr of contract to call
-      const web3 = new Web3(window.ethereum);
-  
-      const contractInstance = new web3.eth.Contract(abi, contractAddress);
-  
-        mint("0xeaDAA842467028D51fAe2f838F56515540F00F79", "2") // check mint func in sol file, you hve to provide the accepted args for it to work 
-        .send({
-          // from: account,
-        })
-        .on("transactionHash", (hash) => {
-          console.log("tx hash", hash);
-        })
-        .once("confirmation", function (confirmationNumber, receipt) {
-          console.log("Transaction confirmed", confirmationNumber);
-          console.log("tx receipt", receipt);
-        })
-        .on("error", function (error, receipt) {
-          console.log("Transaction failed, try again", error);
-        });
-    }
-  
   export default loadWeb3;
